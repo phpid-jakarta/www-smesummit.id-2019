3cd3bd1fcd79b6e72e35a3e1fcfa5e62b270070e
Irfan Maulana <irfan.maulana@bizzy.co.id>
2018-11-21 16:14:00 +0700

    add some dummy layout

--- src/index.js
+++ src/index.js
@@ -1,50 +1,56 @@
-import Rellax from 'rellax'
+import Rellax from 'rellax';
 
-require('intersection-observer')
-require('./index.scss')
+require('intersection-observer');
+require('./index.scss');
 
 document.addEventListener('DOMContentLoaded', function() {
-  new Rellax('.rellax')
+  new Rellax('.rellax');
   if ('IntersectionObserver' in window) {
-    var about = document.querySelector('#conf_title')
+    var about = document.querySelector('#conf_title');
     let lazyImageObserver = new IntersectionObserver(function(entries) {
       entries.forEach(function(entry) {
         if (entry.intersectionRatio <= 0) {
-          document.querySelector('.header_sticky').classList.add('is--have-bg')
+          document.querySelector('.header_sticky').classList.add('is--have-bg');
         } else {
-          document.querySelector('.header_sticky').classList.remove('is--have-bg')
+          document
+            .querySelector('.header_sticky')
+            .classList.remove('is--have-bg');
         }
-      })
-    })
+      });
+    });
 
-    lazyImageObserver.observe(about)
+    lazyImageObserver.observe(about);
   }
 
-  const burgerEl = document.getElementsByClassName('burger')[0]
-  const menuEl = document.getElementsByClassName('mobile-menu')[0]
-  const activeCls = 'is-active'
+  const burgerEl = document.getElementsByClassName('burger')[0];
+  const menuEl = document.getElementsByClassName('mobile-menu')[0];
+  const activeCls = 'is-active';
   const hideMobileMenu = () => {
-    menuEl.classList.remove(activeCls)
-    burgerEl.classList.remove(activeCls)
-  }
+    menuEl.classList.remove(activeCls);
+    burgerEl.classList.remove(activeCls);
+  };
   const showMobileMenu = () => {
-    burgerEl.classList.add(activeCls)
-    menuEl.classList.add(activeCls)
-  }
+    burgerEl.classList.add(activeCls);
+    menuEl.classList.add(activeCls);
+  };
 
   const onClickMenuMobile = () => {
-    hideMobileMenu()
-  }
+    hideMobileMenu();
+  };
 
   const onClickBurger = () => {
-    if(burgerEl.classList.contains(activeCls)) {
-      hideMobileMenu()
+    if (burgerEl.classList.contains(activeCls)) {
+      hideMobileMenu();
     } else {
-      showMobileMenu()
+      showMobileMenu();
     }
-  }
+  };
 
-  burgerEl.addEventListener('click', onClickBurger)
-  menuEl.addEventListener('click', onClickMenuMobile)
-})
+  burgerEl.addEventListener('click', onClickBurger);
+  menuEl.addEventListener('click', onClickMenuMobile);
+});
 
+'serviceWorker' in navigator &&
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js');
+  });
