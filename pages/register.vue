a6d340403d7b6cbda95fce829037e58a8797bd06
Irfan Maulana <irfan.maulana@bizzy.co.id>
2018-12-14 15:49:53 +0700

    sample register

--- pages/register.vue
+++ pages/register.vue
@@ -7,10 +7,16 @@
         REGISTER FOR ATTENDANTS
       </h2>
       <form class="form">
+        <!-- TOKEN -start -->
+        <input
+          :value="_token"
+          class="hidden">
+
         <div class="field">
           <label class="label">Name</label>
           <div class="control">
             <input
+              v-model="formData.name"
               class="input"
               type="text"
               placeholder="Your Name" >
@@ -21,6 +27,7 @@
           <label class="label">Company Name</label>
           <div class="control">
             <input
+              v-model="formData.company_name"
               class="input"
               type="text"
               placeholder="Your Company Name" >
@@ -28,9 +35,21 @@
         </div>
 
         <div class="field">
+          <label class="label">Company Sector</label>
+          <div class="control">
+            <input
+              v-model="formData.company_sector"
+              class="input"
+              type="text"
+              placeholder="Your Company Sector" >
+          </div>
+        </div>
+
+        <div class="field">
           <label class="label">Your Role/Position</label>
           <div class="control">
             <input
+              v-model="formData.position"
               class="input"
               type="text"
               placeholder="Ex: IT, Owner, etc" >
@@ -41,6 +60,7 @@
           <label class="label">Email</label>
           <div class="control">
             <input
+              v-model="formData.email"
               class="input"
               type="email"
               placeholder="Ex: example@mail.com"
@@ -52,9 +72,10 @@
           <label class="label">Phone/WA/Telegram</label>
           <div class="control">
             <input
+              v-model="formData.phone"
               class="input"
               type="text"
-              placeholder="Ex: 0812-123-456-789-00, @your.telegram"
+              placeholder="Ex: 0812-123-456-789-00 or @your.telegram"
             >
           </div>
         </div>
@@ -63,7 +84,7 @@
           <label class="label">Which Sector Do You Want To Get Coached? </label>
           <div class="control">
             <div class="select">
-              <select>
+              <select v-model="formData.coached_sector">
                 <option
                   value=""
                   disabled>Select dropdown</option>
@@ -72,6 +93,7 @@
                 <option>Human Capital</option>
                 <option>Financials</option>
                 <option>Regulations</option>
+                <option>Others</option>
               </select>
             </div>
           </div>
@@ -81,15 +103,49 @@
           <label class="label">Your Current Problem/Case</label>
           <div class="control">
             <textarea
+              v-model="formData.problem_desc"
               class="textarea"
               placeholder="Ex: we already implemented IT in our company but still confused about its scalability"
             />
           </div>
         </div>
 
+        <div class="field">
+          <div>
+            <img
+              v-if="!loading && _captchaImage !== ''"
+              :src="_captchaImage"
+              alt="_captcha">
+          </div>
+          <div>
+            <div
+              :class="{'is-loading': loading}"
+              class="button is-link"
+              @click="refreshCaptcha">
+              Refresh Captcha
+            </div>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Input Captcha</label>
+          <div class="control">
+            <input
+              v-model="formData.captcha_input"
+              class="input"
+              type="text"
+            >
+          </div>
+        </div>
+
         <div class="field is-grouped">
           <div class="control">
-            <button class="button is-link">Submit</button>
+            <div
+              :disabled="loading"
+              class="button is-link"
+              @click="doSubmit">
+              Submit
+            </div>
           </div>
           <div class="control">
             <a
@@ -103,8 +159,67 @@
 </template>
 
 <script>
-export default {
+import { API_ENDPOINT } from '../constant/index'
 
+export default {
+  name: 'RegisterParticipants',
+  data () {
+    return {
+      loading: false,
+      url_api: `${API_ENDPOINT.REGISTER_PARTICIPANT}`,
+      formData: {
+        name: '',
+        company_name: '',
+        position: '',
+        company_sector: '',
+        coached_sector: '',
+        email: '',
+        phone: '',
+        problem_desc: '',
+        captcha_input: ''
+      }
+    }
+  },
+  computed: {
+    _token () {
+      return this.$store.state.token
+    },
+    _captchaImage () {
+      return this.$store.state.captcha
+    }
+  },
+  mounted () {
+    this.requestToken()
+  },
+  methods: {
+    requestToken () {
+      this.loading = true
+      this.$store.dispatch('fetchNewToken', {
+        url: this.url_api,
+        success: () => {
+          setTimeout(() => {
+            this.loading = false
+          }, 1000)
+        }
+      })
+    },
+    refreshCaptcha () {
+      this.requestToken()
+    },
+    doSubmit () {
+      this.loading = true
+      const dataForSubmit = Object.assign({}, this.formData, { _token: this._token })
+      this.$store.dispatch('postRegisterParticipant', {
+        token: this._token,
+        data: dataForSubmit,
+        success: () => {
+          setTimeout(() => {
+            this.loading = false
+          }, 1000)
+        }
+      })
+    }
+  }
 }
 </script>
 
