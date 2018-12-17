06ce7992887ef31d3005a8591c3aa26c7a736f4e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-17 10:22:23 +0000

    Deploy phpid-jakarta/www-smesummit.id-2019 to github.com/phpid-jakarta/www-smesummit.id-2019.git:gh-pages

--- sw.js
+++ sw.js
@@ -2,28 +2,24 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/19da6f6af8a81a81ced4.js",
-    "revision": "622b1ee30e2c31a4477cb3c42721a2dc"
+    "url": "/_nuxt/05c0df0868a1134842ca.js",
+    "revision": "961254335148562317630c91e132a800"
   },
   {
-    "url": "/_nuxt/20d73eef90ffb617cdee.js",
-    "revision": "a40d526531a1eacb99e9bfdaa1a98181"
+    "url": "/_nuxt/204e5473d0f1228bd128.js",
+    "revision": "39a31f165005f3f1f5cbabbce6955732"
   },
   {
     "url": "/_nuxt/22adc1dcdcfff2e903f5.js",
     "revision": "b12f1b6da4131dbf96b3d0047872571e"
   },
   {
-    "url": "/_nuxt/4e4c4acbf38134d164bd.js",
+    "url": "/_nuxt/2b9feff7f7d83ded0e8f.js",
+    "revision": "00dd6d3a7efc943cd8161f2dd4b157c3"
+  },
+  {
+    "url": "/_nuxt/53901a6b1f5732ed89a7.js",
     "revision": "8c389a0ab3d1f8b4b42b0595623f676e"
-  },
-  {
-    "url": "/_nuxt/687d3644a4b8cbbdd984.js",
-    "revision": "7466119b4e86a8deed530ef6a0fc9a90"
-  },
-  {
-    "url": "/_nuxt/6c5442ae2d6cba7384e5.js",
-    "revision": "8c6d22c859d7131856f1251251773a53"
   },
   {
     "url": "/_nuxt/7e3fb9795c0e9081d519.js",
@@ -34,28 +30,32 @@ workbox.precaching.precacheAndRoute([
     "revision": "a4506f2d54b1650f9d5d3c4c9819b0f9"
   },
   {
-    "url": "/_nuxt/8b8e2abda6fd0878e7fc.js",
-    "revision": "7343d15549465db90745838cf7927a79"
+    "url": "/_nuxt/930794e73519ad84509f.js",
+    "revision": "8a650609c643bf05a9c95ef796c158d6"
   },
   {
-    "url": "/_nuxt/a44809ba404e16b62716.js",
-    "revision": "58720be326f4e6b3cfc14156c8f8cd99"
+    "url": "/_nuxt/9c93146746863158965e.js",
+    "revision": "1eb3afa80f8904cc58aa82ef395dd0cd"
   },
   {
-    "url": "/_nuxt/cb70c6526771c43cb371.js",
+    "url": "/_nuxt/9e96c05fcb4585969c03.js",
+    "revision": "8e4bc185dee764a18500900b40280d4e"
+  },
+  {
+    "url": "/_nuxt/cd3ed13c2833fba92db3.js",
     "revision": "71d5072426d8e0032856310e21e1e105"
   },
   {
-    "url": "/_nuxt/cd89d074d6012cbb4fb1.js",
-    "revision": "da4008bd9863308a8019d87adc11b923"
+    "url": "/_nuxt/d64683224cbf43d74412.js",
+    "revision": "d297e7ef39cf4fb130ff9f96654bfe6e"
+  },
+  {
+    "url": "/_nuxt/e12bedd5d28a325179c6.js",
+    "revision": "e4365fd0be29866046b131cd47c80104"
   },
   {
     "url": "/_nuxt/f0f32c015b139abe93bc.js",
     "revision": "70e4d0e53c089bbf362f8f817d726ecb"
-  },
-  {
-    "url": "/_nuxt/f11595f152554f46d5c8.js",
-    "revision": "ec3b7765b4971f4195715f083457c2b7"
   }
 ], {
   "cacheId": "smesummit.id",
