80ea7a5adac13510b8bd45067415f0dd4539cede
Irfan Maulana <irfan.maulana@bizzy.co.id>
2018-12-17 16:10:20 +0700

    add other registration

--- store/index.js
+++ store/index.js
@@ -3,7 +3,13 @@ import {
   API_ENDPOINT
 } from '../constant/index'
 import { getToken } from '../services/token'
-import { registerParticipant } from '../services/register'
+import {
+  registerParticipant,
+  registerSponsor,
+  registerVolunteer,
+  registerCoacher,
+  registerSpeaker
+} from '../services/register'
 
 const createStore = () => {
   return new Vuex.Store({
@@ -31,6 +37,18 @@ const createStore = () => {
       },
       postRegisterParticipant ({ commit, dispatch }, { token, data, success }) {
         registerParticipant(token, data, success)
+      },
+      postRegisterCoacher ({ commit, dispatch }, { token, data, success }) {
+        registerCoacher(token, data, success)
+      },
+      postRegisterVolunteer ({ commit, dispatch }, { token, data, success }) {
+        registerVolunteer(token, data, success)
+      },
+      postRegisterSponsor ({ commit, dispatch }, { token, data, success }) {
+        registerSponsor(token, data, success)
+      },
+      postRegisterSpeaker ({ commit, dispatch }, { token, data, success }) {
+        registerSpeaker(token, data, success)
       }
     }
   })
